decf414ce5f2a63ecda9f435111b427726390cc9
Thiago Faria <thiago_faria@outlook.com>
2025-08-22 16:44:44 -0300

    Arquivos Deletados

--- Create_Table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT exists public.cotacoes (
-  id BIGSERIAL primary key,
-  preco NUMERIC(18,6) not null,
-  ativo TEXT not null,
-  moeda CHAR(3) not null default 'USD',
-  hora_request TIMESTAMPTZ not null,
-  inserido_em  TIMESTAMPTZ not null default now()
-
-);
